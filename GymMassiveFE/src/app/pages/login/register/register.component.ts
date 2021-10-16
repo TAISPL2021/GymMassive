86c9addbfab0c40791cef3a00de6e906c8b6985c
Camilo Andrés Moreno Laverde <camolave12@gmail.com>
2021-10-16 11:55:00 -0500

    Register Component

---
+++ GymMassiveFE/src/app/pages/login/register/register.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+	selector: 'app-register',
+	templateUrl: './register.component.html',
+	styleUrls: [ './register.component.scss' ]
+})
+export class RegisterComponent implements OnInit {
+	hide = true;
+	formGroup = new FormGroup({
+		name: new FormControl(null, Validators.required),
+		last_name: new FormControl(null, Validators.required),
+		birthday: new FormControl(null, Validators.required),
+		document_type: new FormControl(null, Validators.required),
+		document_number: new FormControl(null, Validators.required),
+		phone_number: new FormControl(null, Validators.required),
+		email: new FormControl(null, [ Validators.required, Validators.email ]),
+		password: new FormControl(null, Validators.required)
+	});
+	constructor() {}
+
+	ngOnInit(): void {}
+
+	submit() {
+		debugger;
+	}
+}
