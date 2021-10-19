5e36b38972759306178b89899fa8b748fa09cd03
Camilo Andrés Moreno Laverde <camolave12@gmail.com>
2021-10-19 13:03:17 -0500

    some UI refinements

--- GymMassiveFE/src/app/pages/login/login.component.ts
+++ GymMassiveFE/src/app/pages/login/login.component.ts
@@ -1,40 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { LoginService } from './login.service';
-import { LoginResponse } from './model/LoginResponse';
+import { LoginResponse } from 'src/app/models';
+import { LoginService } from '../../services';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
-  providers: [LoginService]
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: [ './login.component.scss' ],
+	providers: [ LoginService ]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+	formGroup = new FormGroup({
+		email: new FormControl(null, Validators.required),
+		password: new FormControl(null, Validators.required)
+	});
 
-  public email: any = "";
-  public password: any = "";
+	constructor(private loginService: LoginService, private router: Router) {}
 
-  constructor(
-    private loginService: LoginService,
-    private router: Router
-  ) { }
-
-  ngOnInit(): void {
-  }
-
-  login(): void {
-    this.loginService.login(this.email, this.password).subscribe(
-      result => {
-        let loginResponse: LoginResponse = result;
-        sessionStorage.setItem("email", loginResponse.email);
-        sessionStorage.setItem("token", loginResponse.token);
-        sessionStorage.setItem("refreshToken", loginResponse.refreshToken);
-        this.router.navigate(['home']);
-      },
-      error => {
-        console.log(error);
-      }
-    );
-  }
-
+	login(): void {
+		if (this.formGroup.valid) {
+			const { email, password } = this.formGroup.getRawValue();
+			this.loginService.login(email, password).subscribe(
+				(result) => {
+					let loginResponse: LoginResponse = result;
+					sessionStorage.setItem('email', loginResponse.email);
+					sessionStorage.setItem('token', loginResponse.token);
+					sessionStorage.setItem('refreshToken', loginResponse.refreshToken);
+					this.router.navigate([ 'home' ]);
+				},
+				(error) => {
+					console.log(error);
+				}
+			);
+		}
+	}
 }
--- GymMassiveFE/src/app/pages/login/model/LoginResponse.ts
+++
@@ -1,9 +0,0 @@
-export class LoginResponse {
-
-    constructor(
-        public email: string,
-        public token:string,
-        public refreshToken:string
-    ){}
-    
-}
