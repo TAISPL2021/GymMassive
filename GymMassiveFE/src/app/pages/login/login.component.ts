3c48f3f5a37e07ff6541b6ea723b21d3bec0daec
Brayan Guerrero <bguerrero@k12.com>
2021-10-19 09:55:40 -0500

    FE-03 building login request

--- GymMassiveFE/src/app/pages/login/login.component.ts
+++ GymMassiveFE/src/app/pages/login/login.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from './login.service';
+import { LoginResponse } from './model/LoginResponse';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  providers: [LoginService]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email: any = "";
+  public password: any = "";
+
+  constructor(
+    private loginService: LoginService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  login(): void {
+    this.loginService.login(this.email, this.password).subscribe(
+      result => {
+        var loginResponse: LoginResponse = result;
+        console.log(loginResponse)
+        this.router.navigate(['home']);
+      },
+      error => {
+
+      }
+    );
   }
 
 }
--- GymMassiveFE/src/app/pages/login/login.module.ts
+++ GymMassiveFE/src/app/pages/login/login.module.ts
@@ -10,21 +10,24 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { RegisterComponent } from './register/register.component';
 import { MatSelectModule } from '@angular/material/select';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-	declarations: [ LoginComponent, RegisterComponent ],
+	declarations: [LoginComponent, RegisterComponent],
 	imports: [
 		CommonModule,
 		MatIconModule,
-    ReactiveFormsModule,
+		ReactiveFormsModule,
 		MatSelectModule,
 		MatDatepickerModule,
 		MatGridListModule,
 		MatInputModule,
 		MatButtonModule,
 		LoginRoutingModule,
-		MatCardModule
+		MatCardModule,
+		FormsModule,
+		HttpClientModule
 	]
 })
-export class LoginModule {}
+export class LoginModule { }
